6d7d7719b9f66c7d06292de9b716ba695bfbe3ea
allysoncsc <allysoncsc@gmail.com>
2022-04-13 21:31:03 -0300

    aula 65 de setInterval e setTimeout

--- javascript/refazendoexercicios.js
+++ javascript/refazendoexercicios.js
@@ -61,13 +61,26 @@ function retornaHora(date){
     });
 }
 
+// aula 65 setinterval e settimeout
+function mostraHora(){
+    let data = new Date();
 
+    return data.toLocaleTimeString('pt-BR', {
+        hour12:false
+    });
+}
 
+const timer = setInterval(function(){
+    console.log(mostraHora());
+}, 1000);
 
+setTimeout(function(){
+    clearInterval(timer);
+}, 10000);
 
-
-
-
+setTimeout(function(){
+    console.log('Ola mundo!');
+}, 5000);
 
 
 // se a arrow function só tiver uma linha ñ precisa de return
