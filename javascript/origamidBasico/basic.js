77f4c370a4c29b06d4983ab78afb68b67393f66b
Allysoncs <allysoncsc@gmail.com>
2024-06-03 22:57:21 -0300

    começando um site sobe animais para teste js

--- javascript/origamidBasico/basic.js
+++ javascript/origamidBasico/basic.js
@@ -12,4 +12,16 @@ var videoGames = ['Switch','PS4','Xbox','3DS'];
 
 for(var item =0;item < videoGames.length;item++){
     console.log(videoGames[item]);
+    if(videoGames[i] === 'Xbox'){
+        break;
+    }
 }
+
+//percorre o array olhando cada elemento
+videoGames.forEach(function(item){
+    console.log(item)
+})
+
+var idade =19;
+
+var permissao = idade >=18 ? true : false;
